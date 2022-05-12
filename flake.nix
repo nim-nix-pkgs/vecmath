6b1b2548bb30feb25e817fcb23e1bfb5025cffba
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:41 -0300

    chore: re index vecmath

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."vecmath-master".dir   = "master";
+  inputs."vecmath-master".owner = "nim-nix-pkgs";
+  inputs."vecmath-master".ref   = "master";
+  inputs."vecmath-master".repo  = "vecmath";
+  inputs."vecmath-master".type  = "github";
+  inputs."vecmath-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."vecmath-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
